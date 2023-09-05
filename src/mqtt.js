4bad3281eca66278ed51664bdc75d6674df87121
yukuai <1596848231@qq.com>
2023-09-05 13:10:22 +0800

    update .gitignore

---
+++ src/mqtt.js
@@ -0,0 +1,25 @@
+// 引入库
+import mqtt from 'mqtt';
+
+// 连接 MQTT 代理服务器
+const client = mqtt.connect('mqtt://test.mosquitto.org');
+
+// 连接成功的回调函数
+client.on('connect', () => {
+    console.log('Connected to MQTT broker');
+
+    // 订阅主题
+    client.subscribe('hello');
+});
+
+// 接收到消息的回调函数
+client.on('message', (topic, message) => {
+    console.log(`Received message on topic: ${topic}`);
+    console.log(`Message: ${message.toString()}`);
+});
+
+// 断开连接
+client.on('close', () => {
+    console.log('Disconnected from MQTT broker');
+    client.end(); // 结束客户端实例
+});
