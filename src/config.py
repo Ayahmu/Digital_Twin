a2eafde9e3f5e594c5d83f2ff839a9619a6a66e3
yukuai <1596848231@qq.com>
2023-09-07 19:06:48 +0800

    后端代码-ftp部分

---
+++ src/config.py
@@ -0,0 +1,15 @@
+ftp_config = {
+    'host': '192.168.0.174',
+    'port': '21',
+    'user': '15968',
+    'password': 'yukuai000',
+    'compress': False,  # 是否启用数据传输压缩功能
+    'json': {
+        'file_path': '/path/to/json',  # json文件在ftp服务器的路径位置
+        'file_name': 'HydrogenSysInfo.json'       # json文件名
+    }
+}
+
+mqtt_config = {
+    'url': 'mqtt://test.mosquitto.org'
+}
--- src/ftp.py
+++ src/ftp.py
@@ -1,14 +1,38 @@
 from flask import Flask, jsonify, request
+from ftplib import FTP
+from config import ftp_config
+import os
 
 app = Flask(__name__)
-
 
 @app.route('/api/data', methods=['POST'])
 def get_data():
     data = {'message': 'Hello from Flask'}
-    print(request.json)
     return jsonify(data)
+
+def setup():  # 连接ftp服务器并下载json文件
+    try:
+        ftp = FTP(ftp_config['host'])
+        ftp.login(user=ftp_config['user'], passwd=ftp_config['password'])
+        print('成功连接到FTP服务器！')
+    except Exception as e:
+        print(f'连接到FTP服务器失败：{str(e)}')
+
+    try:
+        # 切换到ftp存放json文件的目录
+        # ftp.cwd(ftp_config.get('json').get('file_path'))
+        file_name = ftp_config.get('json').get('file_name')  # 下载json文件名
+        file_path = os.path.join('../public/json', file_name)
+        with open(file_path, 'wb') as f:
+            ftp.retrbinary('RETR ' + file_name, f.write)
+        print('下载文件成功！')
+    except Exception as e:
+        print(f'下载文件失败：{str(e)}')
+
+
+setup()
 
 
 if __name__ == '__main__':
     app.run(host='192.168.0.174', port='8003', debug=True)
+
