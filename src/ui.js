d1881e1e3bf4e0f9fb37fc6227c588f20458fecc
yukuai <1596848231@qq.com>
2023-09-18 23:45:36 +0800

    美化ui，添加动画

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,7 @@
+var leftLabel = document.getElementById("leftLabel");
+var rightLabel = document.getElementById("rightLabel");
+
+
 const back_btn = document.getElementById("back_btn");
 back_btn.addEventListener('click', function (){
     console.log(back_btn.id);
@@ -20,12 +24,22 @@ play_btn.addEventListener('click', function (){
 
 const info_btn = document.getElementById("info_btn");
 info_btn.addEventListener('click', function (){
-    console.log(info_btn.id);
+    if(!leftLabel.style.display || !rightLabel.style.display){
+        leftLabel.style.display = "block";
+        rightLabel.style.display = "block";
+    }
+    leftLabel.classList.remove("left-slide-out");
+    leftLabel.classList.add("left-slide-in");
+    rightLabel.classList.remove("right-slide-out");
+    rightLabel.classList.add("right-slide-in");
 })
 
 const close_btn = document.getElementById("close_btn");
 close_btn.addEventListener('click', function (){
-    console.log(close_btn.id);
+    leftLabel.classList.remove("left-slide-in");
+    leftLabel.classList.add("left-slide-out");
+    rightLabel.classList.remove("right-slide-in");
+    rightLabel.classList.add("right-slide-out");
 })
 
 export {
