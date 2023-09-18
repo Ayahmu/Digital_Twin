31b55d722bf191c193ef0946f02f7d8845f00791
yukuai <1596848231@qq.com>
2023-09-18 12:36:06 +0800

    update

---
+++ src/ui.js
@@ -0,0 +1,29 @@
+const back_btn = document.getElementById("back_btn");
+back_btn.addEventListener('click', function (){
+    console.log(back_btn.id);
+})
+
+const home_btn = document.getElementById("home_btn");
+home_btn.addEventListener('click', function (){
+    console.log(home_btn.id);
+})
+
+const display_btn = document.getElementById("display_btn");
+display_btn.addEventListener('click', function (){
+    console.log(display_btn.id);
+})
+
+const play_btn = document.getElementById("play_btn");
+play_btn.addEventListener('click', function (){
+    console.log(play_btn.id);
+})
+
+const info_btn = document.getElementById("info_btn");
+info_btn.addEventListener('click', function (){
+    console.log(info_btn.id);
+})
+
+const close_btn = document.getElementById("close_btn");
+close_btn.addEventListener('click', function (){
+    console.log(close_btn.id);
+})
